8f0a9a52786509f90724269a3e559dd849d72caf
Rogério Gasi <rtgasi@gmail.com>
2016-09-05 20:34:37 -0300

    Setting up projects to include in main proj.

--- src/Forms/XLabs.Forms/Validation/Action.cs
+++ src/Forms/XLabs.Forms/Validation/Action.cs
@@ -51,7 +51,7 @@ namespace XLabs.Forms.Validation
 				{ typeof(Constraint), new ConstraintTypeConverter() },
 				{ typeof(Font), new FontTypeConverter() },
 				{ typeof(GridLength), new GridLengthTypeConverter() },
-				{ typeof(ImageSource), new ImageSourceConverter() },
+				{ typeof(ImageSource), new XLabs.Forms.Controls.ImageSourceConverter() },
 				{ typeof(Keyboard), new KeyboardTypeConverter() },
 				{ typeof(Point), new PointTypeConverter() },
 				{ typeof(Thickness), new ThicknessTypeConverter() },
